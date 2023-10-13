b9c3baecdfc43ed1998047c3d1bf84c1ada86492
Jason Gautama <147673508+jgautama@users.noreply.github.com>
2023-10-12 20:19:57 -0500

    first commit

---
+++ backend/README.md
@@ -0,0 +1,5 @@
+## Tech Stack:
+
+Python (aiohttp)
+
+SQL
---
+++ ui/index.js
@@ -0,0 +1,14 @@
+var items = [
+{
+    "name": "Sprite",
+    "image": "assets/sprite-can.png",
+    "currency": "USD",
+    "price": 2.49,
+},
+{
+    "name": "Oreo Ice Cream",
+    "image": "assets/",
+    "currency": "USD",
+    "price": 5,
+},
+];
