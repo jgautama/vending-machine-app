6e893789fd26dd3e8bc68e4743d5318217e97998
Jason Gautama <jason.gautama@youversion.com>
2023-10-13 16:05:39 -0500

    add functions to update cart

--- ui/index.js
+++ ui/index.js
@@ -19,9 +19,28 @@ var items = [
 
 // define initial cart
 let custCart = {
-    item: [],
+    items: [],
     total: 0,
 };
 
+function addToCart(item, amount) {
+    addItem(item, amount);
+    updateCart();
+}
 
+function addItem(item, amount) {
+let totalAmount = document.getElementById("cart-total");
+custCart.total += amount;
+custCart.items.push(item);
+totalAmount.innerHTML = custCart.total;
+}
 
+function updateCart() {
+    let summaryCart = document.getElementById("cart-items");
+    summaryCart.innerHTML = "";  
+    custCart.items.forEach((item) => {
+        const cartItem = document.createElement("li");
+        cartItem.textContent = item;
+        summaryCart.appendChild(cartItem);
+    });
+}
