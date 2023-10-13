b97e4008924182a472371b198dafe79a84345957
jasongautama <jgautama98@gmail.com>
2023-10-12 21:42:34 -0500

    remove commented code and minor changes

--- ui/index.js
+++ ui/index.js
@@ -4,11 +4,24 @@ var items = [
     "image": "assets/sprite-can.png",
     "currency": "USD",
     "price": 2.49,
+    "stock": 15,
 },
 {
     "name": "Oreo Ice Cream",
     "image": "assets/",
     "currency": "USD",
     "price": 5,
+    "stock": 20,
 },
 ];
+
+//const vendingMachine = document.getElementById("vending-machine")
+
+// define initial cart
+let custCart = {
+    item: [],
+    total: 0,
+};
+
+
+
