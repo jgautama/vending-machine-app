6c3a396d45941e9056387dd8af5ce5221e1b4275
jasongautama <jgautama98@gmail.com>
2023-10-14 12:32:07 -0500

    remove comment and add gitignore

--- ui/index.js
+++ ui/index.js
@@ -1,21 +1,3 @@
-var items = [
-{
-    "name": "Sprite",
-    "image": "assets/sprite-can.png",
-    "currency": "USD",
-    "price": 2.49,
-    "stock": 15,
-},
-{
-    "name": "Oreo Ice Cream",
-    "image": "assets/oreo-bars.png",
-    "currency": "USD",
-    "price": 5,
-    "stock": 20,
-},
-];
-
-//const vendingMachine = document.getElementById("vending-machine")
 
 // define initial cart
 let custCart = {
